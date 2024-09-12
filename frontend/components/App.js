29a36710721d56492cc83288bcf6f9997d4b8069
Ajani Maragh <amsbstart@gmail.com>
2024-09-11 21:34:44 -0700

    fetched data and did inital mount

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import Form from "./Form"
+import TodoList from "./TodoList"
+import ReactDOM from "react-dom"
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    console.log("constructor invoked")
+    super();
+    this.state = {
+      todos: [],
+      name: '',
+      completed: false
+    }
+  }
+  handleInputChange = (event) => {
+    this.setState({ name: event.target.value })
+  }
+  handleAddTodo = (event) => {
+    event.preventdefault()
+    const newTodo = {
+      name: this.state.name,
+      completed: false,
+    }
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, newTodo],
+      name: '',
+    }))
+  }
+
+  componenetDidMount() {
+    console.log("CDM invoked")
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => this.setState({ todos: data }))
+      .catch(err => console.log(err.message))
+    }
   render() {
-    return null
+    console.log("render invoked")
+    return (
+      <div>
+        <TodoList todos={this.state.todos}/>
+        <Form
+        name={this.state.name}
+        handleInputChange={this.handleInputChange}
+        handleAddTodo={this.handleAddTodo} />
+      </div>
+    )
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Todo from "./Todo"
 
 export default class TodoList extends React.Component {
   render() {
-    return null
+    const { todo } = this.props
+    return (
+      <div>
+        {this.props.todos.map(todo => (
+          <Todo todo={todo} key={todo.id}/>
+        ))}
+      </div>
+    )
   }
 }
